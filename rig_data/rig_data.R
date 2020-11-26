0eef4782b33fc9386cc36c84cb4b6c1e52530ee2
Dirk Wulff <dirkwulff@fp-ws-b210.psycho.unibas.ch>
2020-11-26 16:03:56 +0100

    ..

--- rig_data/rig_data.R
+++ rig_data/rig_data.R
@@ -1,6 +1,7 @@
 require(tidyverse)
 
 college_train = read_csv('1_Data/college_train.csv')
+college_test = read_csv('1_Data/college_test.csv')
 
 m = glm(factor(Private) ~ ., data = college_train, family = 'binomial')
 ps = predict(m,type = 'response')
@@ -11,12 +12,28 @@ p_change = (1-abs(ps-.5)*2)
 set.seed(100)
 change = runif(nrow(college_train),0,1)<p_change
 revers = ifelse(college_train$Private == "Yes", "No", "Yes")
-college_train =  college_train %>% mutate(Grad.Rate = Grad.Rate + rnorm(n(), 0, 20), 
+college_train =  college_train %>% mutate(Grad.Rate = Grad.Rate + rnorm(n(), 0, 10), 
                                           Grad.Rate = ifelse(Grad.Rate<0, 0, Grad.Rate),
-                                          Grad.Rate = ifelse(Grad.Rate<0, 0, Grad.Rate),
+                                          Grad.Rate = ifelse(Grad.Rate>100, 100, Grad.Rate),
                                           Private = ifelse(change,revers,Private))
 
-write_csv(college_train, '1_Data/college_train.csv')
+m = glm(factor(Private) ~ ., data = college_test, family = 'binomial')
+ps = predict(m,type = 'response')
+p_change = (1-abs(ps-.5)*2)
+
+set.seed(100)
+change = runif(nrow(college_test),0,1)<p_change
+revers = ifelse(college_test$Private == "Yes", "No", "Yes")
+college_test =  college_test %>% mutate(Grad.Rate = Grad.Rate + rnorm(n(), 0, 10), 
+                                          Grad.Rate = ifelse(Grad.Rate<0, 0, Grad.Rate),
+                                          Grad.Rate = ifelse(Grad.Rate>100, 100, Grad.Rate),
+                                          Private = ifelse(change,revers,Private))
+
+#write_csv(college_train, '1_Data/college_train.csv')
 write_csv(college_train, '_sessions/Fitting/1_Data/college_train.csv')
 write_csv(college_train, '_sessions/Prediction/1_Data/college_train.csv')
 write_csv(college_train, '_sessions/Optimization/1_Data/college_train.csv')
+#write_csv(college_train, '1_Data/college_train.csv')
+write_csv(college_test, '_sessions/Fitting/1_Data/college_test.csv')
+write_csv(college_test, '_sessions/Prediction/1_Data/college_test.csv')
+write_csv(college_test, '_sessions/Optimization/1_Data/college_test.csv')
